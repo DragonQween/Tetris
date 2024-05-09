069e95d19330452e6951b3fe044088df2664c203
DragonQween <ayleneamyvega@gmail.com>
2024-05-09 11:50:10 +0200

    Agregados assets y ventana más grande

--- tetromino.py
+++ tetromino.py
@@ -9,9 +9,10 @@ class Block(pg.sprite.Sprite):
         self.alive = True
 
         super().__init__(tetromino.tetris.sprite_group)
-        self.image = pg.Surface([TILE_SIZE, TILE_SIZE])
-        pg.draw.rect(self.image, 'pink', (1, 1, TILE_SIZE -
-                     2, TILE_SIZE - 2), border_radius=8)
+        self.image = tetromino.image
+        # self.image = pg.Surface([TILE_SIZE, TILE_SIZE])
+        # pg.draw.rect(self.image, 'pink', (1, 1, TILE_SIZE -
+        #             2, TILE_SIZE - 2), border_radius=8)
         self.rect = self.image.get_rect()
 
     def is_alive(self):
@@ -42,6 +43,7 @@ class Tetromino:
     def __init__(self, tetris):
         self.tetris = tetris
         self.shape = random.choice(list(TETROMINOES.keys()))
+        self.image = random.choice(tetris.app.images)
         self.blocks = [Block(self, pos) for pos in TETROMINOES[self.shape]]
         self.landing = False
 
